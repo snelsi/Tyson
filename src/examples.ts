386a3c77202faa6f63d31754bb403c134e1ddab0
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-05-23 22:48:50 +0300

    update examples

--- src/examples.ts
+++ src/examples.ts
@@ -1,85 +1,89 @@
 export const examples = [
   // Example 1
-  `  // Roman Zhuravlov, TR-71, 2020
+  `// Roman Zhuravlov, TR-71, 2020
 
-  // Загальний приклад, що демонструє всі можливості розробленої мови Tyson:
-  // • Математичні вирази
-  // • Логічні вирази
-  // • Цикли for
-  // • Цикли while
-  // • Цикли do while
+// Загальний приклад, що демонструє всі можливості розробленої мови Tyson:
+// • Математичні вирази
+// • Логічні вирази
+// • Цикли for
+// • Цикли while
+// • Цикли do while
 
-  {
-    key = 123;
-  }
-
+{
   key = 123;
-  myVariable = 7;
+}
 
-  for (i = 0; i < myVariable; i += 1) {
-    key = 123;
-  }
+'single quote string';
+"double quote string";
 
-  if (1 == 2) {
-    key = 1;
-  }
-  else if (2 == 2) {
-    key = 2;
-  }
-  else {
-    key = 3;
-  }
-  `,
+var integer = 123;
+let double = 1.23;
+const myVariable = 7;
+
+for (i = 0; i < myVariable; i += 1) {
+  key = 123;
+}
+
+if (1 == 2) {
+  key = 1;
+}
+else if (2 == 2) {
+  key = 2;
+}
+else {
+  key = 3;
+}
+`,
 
   // Example 2
-  `  key = 123;
-  myVariable = 7 + 2;
-  `,
+  `key = 123;
+myVariable = 7 + 2;
+`,
 
   // Example 4
-  `  for (i = 0; i < myVariable; i += 1) {
-    key = 123;
-  }
-  `,
+  `for (i = 0; i < myVariable; i += 1) {
+  key = 123;
+}
+`,
 
   // Example 5
-  `  if (1 == 2) {
-    key = 1;
-  }`,
+  `if (1 == 2) {
+  key = 1;
+}`,
 
   // Example 6
-  `  if (1 == 2) {
-    key = 1;
-  }
-  else if (2 == 2) {
-    key = 2;
-  }
-  else {
-    key = 3;
-  }`,
+  `if (1 == 2) {
+  key = 1;
+}
+else if (2 == 2) {
+  key = 2;
+}
+else {
+  key = 3;
+}`,
 
   // Example 7
-  `  let i = 0;
-    while(i < 4) {
-      i++;
-    }
-  `,
+  `let i = 0;
+while(i < 4) {
+  i++;
+}
+`,
 
   // Example 8
-  `  let i = 0;
-  do {
-    i++;
-  } while(i < 4);
-  `,
+  `let i = 0;
+do {
+  i++;
+} while(i < 4);
+`,
 
   // Example 9
-  `  for (i = 0; i < myVariable i += 1) {
-    key = 123;
-  }
-  `,
+  `for (i = 0; i < myVariable i += 1) {
+  key = 123;
+}
+`,
 
   // Example 10
-  `  key = 123;
-  myVariable = 7 + 2;
-  `,
+  `key = 123;
+myVariable = 7 + 2;
+`,
 ];
